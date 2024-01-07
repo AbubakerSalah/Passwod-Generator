3dd54072c2b7696ea50f574161aa68bfd2b3a2b6
Abubaker <137734278+AbubakerSalah@users.noreply.github.com>
2024-01-07 20:56:22 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-let characters1 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters1 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let characters2 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-let inputEl1 = document.getElementById("input-el1")
-let inputEl2 = document.getElementById("input-el2")
+
+const inputEl1 = document.getElementById("input-el1")
+const inputEl2 = document.getElementById("input-el2")
 
 
 
@@ -13,20 +12,11 @@ let inputEl2 = document.getElementById("input-el2")
 
 
  function newButton() {
-      let result= ""
-      let length = 12
-      for (let i = 0; i< length; i++) {
-            let randomIndex1 = Math.floor(Math.random() * characters1.length)
-            let randomCharacter1 = characters1[randomIndex1]
-            inputEl1.textContent += randomCharacter1
-     
-     let randomIndex2 = Math.floor(Math.random() * characters2.length)
-     let randomCharacter2 = characters2[randomIndex2]
-     inputEl2.textContent += randomCharacter2
-     
-     
-      
-      result += randomCharacter1 + randomCharacter2
+let result= ""
+let length = 12
+for(let i = 0; i< length *2 ; i++) {
+     const randomIndex = Math.floor(Math.random() * characters1.length)
+     result += characters1 [randomIndex]
 
       }
       inputEl1.value = result.slice(0, length)
