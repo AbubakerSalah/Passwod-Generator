0f2629c1bce8132acd319db48d98f621b97a436c
Abubaker <137734278+AbubakerSalah@users.noreply.github.com>
2024-07-30 12:00:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
+"S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+"p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7",
+ "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=",
+ "{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-const inputEl1 = document.getElementById("input-el1")
-const inputEl2 = document.getElementById("input-el2")
+const btnEl = document.getElementById("btn-el")
+const utputEl = document.getElementById("utput-el")
+const utputEl2 = document.getElementById("utput-el2")
+const inputValue = document.getElementById("input-value")
 
-function newButton() {
-let result= ""
-let length = 12
-for(let i = 0; i< length *2; i++) {
-     const randomIndex = Math.floor(Math.random() * characters.length)
-     result += characters [randomIndex]
 
+function generateRandomPassword(length) {
+      let password = ""
+      
+      for(let i = 0; i < length; i++) {
+            let randomIndex = Math.floor(Math.random()* characters.length)
+            password += characters [randomIndex]
+            
+      } 
+      return password
+      
+}
+
+function generatePasswords() {
+      let length = parseInt(inputValue.value)
+      if(isNaN(length) || length < 1) {
+            alert("Enter a number greater than 0")
+            return
       }
-      inputEl1.value = result.slice(0, length)
-      inputEl2.value = result.slice(length)
       
       
- }
- 
- 
- 
- 
+
+      const password = generateRandomPassword(length)
+      const password2 = generateRandomPassword(length)
+
+      utputEl.textContent = password
+      utputEl2.textContent = password2
+}
+
+
+btnEl.addEventListener("click", function() {
+      generatePasswords()
+      
+})
+
+
